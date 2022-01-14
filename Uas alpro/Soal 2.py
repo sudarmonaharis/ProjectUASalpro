f24eaae45df7b0fc38c84dec373a546518a35fce
sudarmon aharis <92767468+sudarmonaharis@users.noreply.github.com>
2022-01-14 15:22:06 +0700

    Add files via upload

---
+++ Uas alpro/Soal 2.py
@@ -0,0 +1,12 @@
+a =int(input("a:"))
+b =int(input("b:"))
+c =int(input("c:"))
+maks =0
+print(a,b,c)
+if =a>b:
+    maks = a
+else:
+    maks = b
+if c > maks:
+    maks = c
+print("terbesar: ",maks)
---
+++ Uas alpro/Soal 4.py
@@ -0,0 +1,7 @@
+data = []
+for i in range_(2):
+    x = int(input('masukan bilangan:'))
+    data.append(x)
+print('data sebelum diurutkan:'.data)
+list.sort(data)
+print('Data setelah diurutkan'.data)
